2a95cf14338ff258cd0f7ebc1bc2d528d0bbe061
최장호 <hmchoi651@naver.com>
2020-03-26 00:51:48 +0900

    [추가 , 업데이트] frontend 파일 추가 , constants 파일 추가
    html -> frontend 이동
    constants -> message , RegExp 파일 추가

---
+++ frontend/constants/RegExp.js
@@ -0,0 +1,5 @@
+const RegExp = {
+  ID: /^[0-9a-z_-]{5,20}$/
+};
+
+export default RegExp;
---
+++ frontend/constants/message.js
@@ -0,0 +1,18 @@
+export const ID_MESSAGE = {
+  DEFAULT: `아이디를 입력해 주세요.`,
+  SUCCESS: `사용 가능한 아이디 입니다.`,
+  ERROR_REGEXP: `5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.`,
+  ERROR_OVERLAP: `이미 사용중이거나 탈퇴한 아이디입니다.`
+};
+
+export const PASSWORD_MESSAGE = {
+  DEFAULT: `비밀번호를 입력해 주세요`,
+  SUCCESS: `사용 가능한 비밀번호 입니다.`,
+  ERROR_REGEXP: `8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.`
+};
+
+export const EMAIL_MESSAGE = {
+  DEFAULT: `이메일을 입력해 주세요.`,
+  SUCCESS: `사용 가능한 이메일 입니다.`,
+  ERROR_OVERLAP: `이미 사용중이거나 탈퇴한 이메일입니다.`
+};
