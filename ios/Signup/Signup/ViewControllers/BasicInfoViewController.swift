54ecc348486d5146748e8cd216f1af3bc6a4e7bd
kjoonk <kjoonk@gmail.com>
2020-03-26 23:42:11 +0900

    textField를 벗어날 때 키보드 숨기기 테스트 구현

--- ios/Signup/Signup/ViewControllers/BasicInfoViewController.swift
+++ ios/Signup/Signup/ViewControllers/BasicInfoViewController.swift
@@ -2,13 +2,26 @@ import UIKit
 
 class BasicInfoViewController: UIViewController {
 
+    @IBOutlet weak var idField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        idField.delegate = self
     }
 
     @IBAction func backToBasicInfo(_ segue: UIStoryboardSegue) {
         print("back To Basic Info")
     }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.idField.resignFirstResponder()
+    }
 }
 
+extension BasicInfoViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.idField.resignFirstResponder()
+
+        return true
+    }
+}
