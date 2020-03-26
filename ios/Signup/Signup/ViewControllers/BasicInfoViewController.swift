623b22ec3583cb82920612e4fb2f0e80b64c8961
kjoonk <kjoonk@gmail.com>
2020-03-26 20:47:57 +0900

    두 번째 화면 전환 구현
    - Action segue, Unwind segue 사용

---
+++ ios/Signup/Signup/ViewControllers/BasicInfoViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class BasicInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func backToBasicInfo(_ segue: UIStoryboardSegue) {
+        print("back To Basic Info")
+    }
+}
+
